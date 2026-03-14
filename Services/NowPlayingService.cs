c2127058f6339363caf607b5fe211c1fceb0b41a
CheapNud <brechtvandeninden@hotmail.com>
2026-03-14 13:11:48 +0100

    minor fixes

--- Services/NowPlayingService.cs
+++ Services/NowPlayingService.cs
@@ -2,6 +2,11 @@ namespace CheapNights.Services;
 
 public class NowPlayingService
 {
-    public event Action? OnChange;
-    public void NotifyChanged() => OnChange?.Invoke();
+    public event Func<Task>? OnChange;
+
+    public async Task NotifyChangedAsync()
+    {
+        if (OnChange is not null)
+            await OnChange.Invoke();
+    }
 }
