ee85e9cd6756a08ad2500318525c2129ff921281
CheapNud <brechtvandeninden@hotmail.com>
2026-03-14 17:06:33 +0100

    selectors

--- Models/Category.cs
+++ Models/Category.cs
@@ -5,8 +5,8 @@ namespace CheapNights.Models;
 public class Category : IEntityCode
 {
     public int Id { get; set; }
-    public required string Code { get; set; }
-    public required string Name { get; set; }
+    public string Code { get; set; } = "";
+    public string Name { get; set; } = "";
     public string? BadgeColor { get; set; }
     public int SortOrder { get; set; }
 }
